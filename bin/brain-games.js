18cf472ac8622ecc75042d266591560d9c8625a5
Teo <teobox11@gmail.com>
2020-12-16 03:32:29 +0300

    realized prompt interaction by native code

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,6 @@
+#!/usr/bin/env node
+
+import { askName } from '../src/cli.js';
+
 console.log('Welcome to Brain Games!');
+askName();
---
+++ src/cli.js
@@ -0,0 +1,21 @@
+import prompt from 'prompt';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+
+function askName() {
+	rl.question('May I have your name? ', function(name) {
+		console.log(`Hello, ${name}!`);
+		rl.close();
+	});
+	
+	rl.on("close", function() {
+		process.exit(0);
+	})
+};
+
+export { askName };
