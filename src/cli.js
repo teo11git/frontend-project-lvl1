3d3a30b6b58b2440ee316d11fcf7f15f0190c981
Teo <teobox11@gmail.com>
2020-12-16 04:52:36 +0300

    install eslint

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,20 @@
-import prompt from 'prompt';
+// import prompt from 'prompt';
 import readline from 'readline';
 
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-
 function askName() {
-	rl.question('May I have your name? ', function(name) {
-		console.log(`Hello, ${name}!`);
-		rl.close();
-	});
-	
-	rl.on("close", function() {
-		process.exit(0);
-	})
+  rl.question('May I have your name? ', function(name) {
+    console.log(`Hello, ${name}!`);
+    rl.close();
+  });
+
+  rl.on('close', function () {
+    process.exit(0);
+  })
 };
 
 export { askName };
