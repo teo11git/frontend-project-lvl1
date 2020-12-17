d61509ab42dc9c6f41dbcaae907ea13509688972
Teo <teobox11@gmail.com>
2020-12-17 23:42:23 +0300

    Something work in Async cli

--- src/cli.js
+++ src/cli.js
@@ -1,20 +1,49 @@
 // import prompt from 'prompt';
 import readline from 'readline';
 
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-function askName() {
-  rl.question('May I have your name? ', function(name) {
-    console.log(`Hello, ${name}!`);
-    rl.close();
-  });
+const parse = () => {
+  return new Promise((resolve, reject) => {
+    let data = [];
 
-  rl.on('close', function () {
-    process.exit(0);
-  })
+   rl.on('line', (line) => {
+      data.push(line);
+      rl.close();
+    });
+
+    rl.on('close', () => {
+      resolve(data);
+    });
+  });
 };
 
-export { askName };
+const greetings = 'May i have your name?';
+
+async function askQuestion(query, isNeedExit = true) {
+
+  rl.prompt(false);
+  console.log(`${query} `);
+  const value = await parse();
+
+  if (query === greetings) {
+    console.log(`Hello, ${value}!`);
+  }
+
+  if (isNeedExit) {
+    process.exit(0);
+  }
+
+  return new Promise ((resolve, reject) => {
+    resolve(value);
+  });
+
+};
+
+askQuestion(greetings);
+
+export { askQuestion };
