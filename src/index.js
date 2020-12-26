ad963f1142a24dd76ac9849c44a88b2b54dc70eb
Teo <teobox11@gmail.com>
2020-12-27 00:48:08 +0300

    index.js again

--- src/index.js
+++ src/index.js
@@ -1,30 +1,32 @@
 import askUser from './cli/ask-user.js';
 
-const roundCount = 3;
-
 const errorMessage = ' is wrong answer ;( Correct answer was ';
 const endMessage = 'Let\'s try again, ';
 const winnerMessage = 'Congratulations, ';
 
+const roundCount = 3;
+
 const { log } = console;
 
-export default async (gameFn, rules) => {
+export default async (makeQuestion, rules) => {
   log('Welcome to the Brain Games!');
   const name = await askUser();
-  log(rules);
-  let i = 0;
-  do {
-    const [quest, rightAnswer] = gameFn();
+  log(`${rules}`);
+  const startGame = async (counter) => {
+    if (counter === 0) {
+      log(`${winnerMessage}${name}`);
+      return;
+    }
+    const [quest, rightAnswer] = makeQuestion();
     log(`Question: ${quest}`);
     const userAnswer = await askUser('Your answer: ');
     if (userAnswer === String(rightAnswer)) {
       log('Correct!');
+      await startGame(counter - 1);
     } else {
       log(`${userAnswer}${errorMessage}${rightAnswer}.`);
       log(`${endMessage}${name}!`);
-      return;
     }
-    i ++;
-  } while (i < roundCount)
-  log(`${winnerMessage}${name}`);
+  };
+  await startGame(roundCount);
 };
