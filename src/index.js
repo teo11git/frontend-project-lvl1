af10444feef3fffd8e54a50ca4cd8644d46bde73
Teo <teobox11@gmail.com>
2020-12-27 03:36:27 +0300

    little change in index

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ export default async (makeQuestion, rules) => {
   log('Welcome to the Brain Games!');
   const name = await askUser();
   log(`${rules}`);
-  const startGame = async (roundCount = 3) => {
+  const startGame = async (nm, roundCount = 3) => {
     if (roundCount === 0) {
       log(`Congratulations, ${name}!`);
       return;
@@ -16,11 +16,11 @@ export default async (makeQuestion, rules) => {
     const userAnswer = await askUser('Your answer: ');
     if (userAnswer === rightAnswer) {
       log('Correct!');
-      await startGame(roundCount - 1);
+      await startGame(nm, roundCount - 1);
     } else {
       log(`'${userAnswer}' is wrong answer ;( Correct answer was '${rightAnswer}'.`);
       log(`Let's try again, ${name}!`);
     }
   };
-  await startGame();
+  await startGame(name);
 };
