7d43e49bf6c6aa3c4fba9af9c1613a39072f431d
Teo <teobox11@gmail.com>
2021-01-04 13:45:39 +0300

    changed index to loop

--- src/index.js
+++ src/index.js
@@ -6,21 +6,37 @@ export default async (makeQuestion, rules) => {
   log('Welcome to the Brain Games!');
   const name = await askUser();
   log(rules);
-  const startGame = async (roundCount = 3) => {
-    if (roundCount === 0) {
-      log(`Congratulations, ${name}!`);
-      return;
-    }
+  let roundCount = 3;
+
+  while (roundCount !== 0) {
     const [quest, rightAnswer] = makeQuestion();
     log(`Question: ${quest}`);
     const userAnswer = await askUser('Your answer: ');
-    if (userAnswer === rightAnswer) {
-      log('Correct!');
-      await startGame(roundCount - 1);
-    } else {
+    if (userAnswer !== rightAnswer) {
       log(`'${userAnswer}' is wrong answer ;( Correct answer was '${rightAnswer}'.`);
       log(`Let's try again, ${name}!`);
+      return;
     }
-  };
-  await startGame();
+    log('Correct!');
+    roundCount -= 1;
+  }
+  log(`Congratulations, ${name}!`);
+
+//  const startGame = async (roundCount = 3) => {
+//    if (roundCount === 0) {
+//      log(`Congratulations, ${name}!`);
+//      return;
+//    }
+//    const [quest, rightAnswer] = makeQuestion();
+//    log(`Question: ${quest}`);
+//    const userAnswer = await askUser('Your answer: ');
+//    if (userAnswer === rightAnswer) {
+//      log('Correct!');
+//      await startGame(roundCount - 1);
+//    } else {
+//      log(`'${userAnswer}' is wrong answer ;( Correct answer was '${rightAnswer}'.`);
+//      log(`Let's try again, ${name}!`);
+//    }
+//  };
+//  await startGame();
 };
