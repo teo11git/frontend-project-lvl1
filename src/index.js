ee48660269af39572640c3f70c36c3f77adbf47e
Teo <teobox11@gmail.com>
2021-01-18 16:23:11 +0300

    delete readline and changed index

--- src/index.js
+++ src/index.js
@@ -21,22 +21,5 @@ export default async (makeQuestion, rules) => {
     roundCount -= 1;
   }
   log(`Congratulations, ${name}!`);
-
-//  const startGame = async (roundCount = 3) => {
-//    if (roundCount === 0) {
-//      log(`Congratulations, ${name}!`);
-//      return;
-//    }
-//    const [quest, rightAnswer] = makeQuestion();
-//    log(`Question: ${quest}`);
-//    const userAnswer = await askUser('Your answer: ');
-//    if (userAnswer === rightAnswer) {
-//      log('Correct!');
-//      await startGame(roundCount - 1);
-//    } else {
-//      log(`'${userAnswer}' is wrong answer ;( Correct answer was '${rightAnswer}'.`);
-//      log(`Let's try again, ${name}!`);
-//    }
-//  };
-//  await startGame();
+  return;
 };
